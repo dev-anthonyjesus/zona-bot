7bc1c368a15133f0c72bc8fe967c1c4a6ef95a9d
Anthony Jesus <anthony.ondriver@gmail.com>
2026-09-16 16:23:18 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,36 @@
-print("🟢 ZONA BOT iniciado!")
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/")
+VERIFY_TOKEN = "ZONA_BOT_2026"
+
+
+@app.route("/", methods=["GET"])
 def home():
     return "🟢 ZONA BOT online!"
+
+
+@app.route("/webhook", methods=["GET"])
+def verify_webhook():
+    mode = request.args.get("hub.mode")
+    token = request.args.get("hub.verify_token")
+    challenge = request.args.get("hub.challenge")
+
+    if mode == "subscribe" and token == VERIFY_TOKEN:
+        return challenge, 200
+
+    return "Token inválido", 403
+
+
+@app.route("/webhook", methods=["POST"])
+def receive_message():
+    data = request.get_json()
+
+    print("📩 Mensagem recebida:")
+    print(data)
+
+    return "EVENT_RECEIVED", 200
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
